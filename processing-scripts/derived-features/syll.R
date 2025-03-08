b60314ea0a78b4935cbeab63c72565929b5d20a9
davidainman <havennah@gmail.com>
2025-03-08 10:25:09 +0100

    Spelling out values in Syll-04, Capitalizing values in Syll-05
    This required changes to values, parameters, codes, as well as syll.R (for errors), and Syll.md (for the spell-out of Syll-04).
    
    Took the chance to correct an error from a previous checkin on MonPl about wich1262.

--- processing-scripts/derived-features/syll.R
+++ processing-scripts/derived-features/syll.R
@@ -8,14 +8,14 @@ processSyllable <- function(featurestable, lgnames, warnings, errors) {
   errorsList <- suppressWarnings(dplyr::filter(featurestable, !is.na(as.numeric(`Syll-01`)) & !is.na(as.numeric(`Syll-02`)) & !is.na(as.numeric(`Syll-03`)) & (as.numeric(`Syll-03`) != as.numeric(`Syll-01`) + as.numeric(`Syll-02`))))
   addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', 'Syll-03 != Syll-01 + Syll-02', lgnames)
   
-  errorsList <- suppressWarnings(dplyr::filter(featurestable, !is.na(as.numeric(`Syll-02`)) & (`Syll-02` >= 1 & `Syll-04` == 's')))
-  addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-02 >= 1 and Syll-04 = 's'", lgnames)
+  errorsList <- suppressWarnings(dplyr::filter(featurestable, !is.na(as.numeric(`Syll-02`)) & (`Syll-02` >= 1 & `Syll-04` == 'simple')))
+  addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-02 >= 1 and Syll-04 = 'simple'", lgnames)
   
-  errorsList <- suppressWarnings(dplyr::filter(featurestable, !is.na(as.numeric(`Syll-02`)) & !is.na(as.numeric(`Syll-03`)) & (`Syll-01` >= 3 | `Syll-02` >= 2) & (`Syll-04` == 's' | `Syll-04` == 'mc')))
-  addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-01 >= 3 or Syll-02 >= 2 ==> Syll-04 != 's' or 'mc'", lgnames)
+  errorsList <- suppressWarnings(dplyr::filter(featurestable, !is.na(as.numeric(`Syll-02`)) & !is.na(as.numeric(`Syll-03`)) & (`Syll-01` >= 3 | `Syll-02` >= 2) & (`Syll-04` == 'simple' | `Syll-04` == 'moderately complex')))
+  addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-01 >= 3 or Syll-02 >= 2 ==> Syll-04 != 'simple' or 'moderately complex'", lgnames)
   
-  errorsList <- suppressWarnings(dplyr::filter(featurestable, !is.na(as.numeric(`Syll-01`)) & !is.na(as.numeric(`Syll-02`)) & (`Syll-01` >= 4 | `Syll-02` >= 4) & (`Syll-04` != 'hc')))
-  addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-01 >= 4 or Syll-02 >= 4 ==> Syll-04 == 'hc'", lgnames)
+  errorsList <- suppressWarnings(dplyr::filter(featurestable, !is.na(as.numeric(`Syll-01`)) & !is.na(as.numeric(`Syll-02`)) & (`Syll-01` >= 4 | `Syll-02` >= 4) & (`Syll-04` != 'highly complex')))
+  addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-01 >= 4 or Syll-02 >= 4 ==> Syll-04 == 'highly complex'", lgnames)
   
   errorsList <- suppressWarnings(dplyr::filter(featurestable, (!is.na(as.numeric(`Syll-02`)) & `Syll-02` == 0 & `Syll-06` != 'nocoda') | (`Syll-06` == 'nocoda' & `Syll-02` != 0)))
   addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-02 == 0 <=> Syll-06 == 'nocoda'", lgnames)
@@ -23,11 +23,11 @@ processSyllable <- function(featurestable, lgnames, warnings, errors) {
   errorsList <- suppressWarnings(dplyr::filter(featurestable, (!is.na(as.numeric(`Syll-02`)) & `Syll-02` == 1 & (`Syll-06` != 'sonorant' & `Syll-06` != 'simple')) | ((`Syll-06` == 'sonorant' | `Syll-06` == 'simple') & `Syll-02` != 1)))
   addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-02 == 1 <=> Syll-06 == 'sonorant' | Syll-06 == 'simple'", lgnames)
   
-  errorsList <- dplyr::filter(featurestable, (`Syll-05` == 'cg' & `Syll-03` == 's'))
-  addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-05 == cg => Syll-03 != 's'", lgnames)
+  errorsList <- dplyr::filter(featurestable, (`Syll-05` == 'CG' & `Syll-03` == 'simple'))
+  addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-05 == CG => Syll-03 != 'simple'", lgnames)
   
-  errorsList <- dplyr::filter(featurestable, (`Syll-05` == 'ccplus' & (`Syll-03` == 's' | `Syll-03` == 'mc')))
-  addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-05 == ccplus => Syll-03 != 's' & Syll-03 != 'mc'", lgnames)
+  errorsList <- dplyr::filter(featurestable, (`Syll-05` == 'CCplus' & (`Syll-03` == 'simple' | `Syll-03` == 'moderately complex')))
+  addErrorsOrWarnings('Error', errorsList, errorsInt, 'Syll', "Syll-05 == CCplus => Syll-03 != 'simple' & Syll-03 != 'moderately complex'", lgnames)
   
   #warnings
   warningsInt <- warnings
